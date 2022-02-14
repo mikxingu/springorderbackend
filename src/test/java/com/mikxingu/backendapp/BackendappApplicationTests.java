4cb963f2c7933282ba07361430d09f423eea615d
mikxingu <michelalvs@gmail.com>
2022-02-14 20:40:12 -0300

    Innitial Project Commit

---
+++ src/test/java/com/mikxingu/backendapp/BackendappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikxingu.backendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
