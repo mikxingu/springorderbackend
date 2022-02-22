9d1a23713f3c44d59f1549e78b25f46e61c42641
mikxingu <michelalvs@gmail.com>
2022-02-21 21:09:41 -0300

    Added service layer

---
+++ src/main/java/com/mikxingu/backendapp/services/UserService.java
@@ -0,0 +1,30 @@
+package com.mikxingu.backendapp.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mikxingu.backendapp.entities.User;
+import com.mikxingu.backendapp.repositores.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		
+		return obj.get();
+	}
+	
+
+}
